fd6c5bcfa0acbeb457c5fbed26efd86acc443437
artgonzalez <artgonzalezjr@gmail.com>
2021-02-13 17:17:25 -0600

    223 valid identifiers

---
+++ src/examples/002Chapter/2-2-3-identifiers/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{   
+    int name, Name, NamE, NAME;//four variables defined
+
+    //INVALID int double 3.14;
+    int _;
+    //INVALID int catch-22;
+    //INVALID int 1_or_2;
+    double Double = 3.14;
+
+    return 0;
+}
