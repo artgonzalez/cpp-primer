40d66ba635671bfebef0adb3cb82f70fcd7c8247
artgonzalez <artgonzalezjr@gmail.com>
2021-02-12 22:47:12 -0600

    221 variable definitions

---
+++ src/examples/002Chapter/2-2-1-variable-definition/variable_def.cpp
@@ -0,0 +1,37 @@
+#include "variable_def.h"
+#include<iostream>
+
+bool get_boolean_value(int num)
+{
+    return num;
+}
+
+int get_int_val_from_bool(bool flag)
+{
+    return flag;
+}
+
+unsigned char get_unsigned_char(int num)
+{
+    return num;
+}
+
+signed char get_signed_char(int num)
+{
+    return num;
+}
+
+int add_int_unsigned_int(int num1, unsigned int num2)
+{
+    return num1 + num2;
+}
+
+unsigned int subtract_unsigned_int(unsigned int num1, unsigned int num2)
+{
+    return num2 - num1;
+}
+
+int subtract_int_unsigned_int(int num1, unsigned int num2)
+{
+    return num2 - num1;
+}
---
+++ test/examples/002Chapter/02_example_tests.cpp
@@ -0,0 +1,41 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "variable_def.h"
+
+TEST_CASE("Test setting int values to bool variable", "int 0 bool false any other value true")
+{
+    REQUIRE(get_boolean_value(0) == false);
+    REQUIRE(get_boolean_value(1) == true);
+}
+
+TEST_CASE("Test bool value in int", "true returns 1 false returns 0")
+{
+    REQUIRE(get_int_val_from_bool(true) == 1);
+    REQUIRE(get_int_val_from_bool(false) == 0);
+}
+
+TEST_CASE("Test unsigned char", "use value less than 0")
+{
+    REQUIRE(get_unsigned_char(-1) == 255);
+}
+
+TEST_CASE("Test signed char", "256 returns undefined")
+{
+    REQUIRE(get_signed_char(256) == 0);
+}
+
+TEST_CASE("Test add int and unsigned int", "")
+{
+    REQUIRE(add_int_unsigned_int(-42, 10) == -32);
+}
+
+TEST_CASE("Test add unsigned int and unsigned int", "")
+{
+    REQUIRE(subtract_unsigned_int(10, 42) == 32);
+    REQUIRE(subtract_unsigned_int(42, 10) == 4294967264);
+}
+
+TEST_CASE("Test subtract unsigned int and int", "")
+{
+    REQUIRE(subtract_int_unsigned_int(10, 10) == 0);
+}
