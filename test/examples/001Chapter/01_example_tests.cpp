f77cac3b8a7d9d00aaa13b0c6bd6820e2d3a2f21
artgonzalez <artgonzalezjr@gmail.com>
2021-02-12 20:46:16 -0600

    test case sum while and or

--- test/examples/001Chapter/01_example_tests.cpp
+++ test/examples/001Chapter/01_example_tests.cpp
@@ -1,9 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "while_sum.h"
+#include "for_sum.h"
 
 TEST_CASE("Test while sum function")
 {
     REQUIRE(sum_while(2) == 3);
     REQUIRE(sum_while(3) == 6);
+}
+
+TEST_CASE("Test for sum function")
+{
+    REQUIRE(sum_for(2) == 3);
+    REQUIRE(sum_for(3) == 6);
 }
