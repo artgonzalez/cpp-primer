d29895e33eea8354d8c167baef747dba13249c50
artgonzalez <artgonzalezjr@gmail.com>
2021-02-12 23:29:15 -0600

    212 type conversions

---
+++ src/examples/002Chapter/2-1-2-type-conversions/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "type_conversion.h"
+
+int main()
+{
+    unsigned u = 10, u2 = 42;
+    std::cout << subtract_unsigned_int(u, u2) << std::endl;
+    std::cout << subtract_unsigned_int(u2, u) << std::endl;
+    int i = 10, i2 = 42;
+    std::cout << i2 - i << std::endl;
+    std::cout << i - i2 << std::endl;
+    std::cout << i - u << std::endl;
+    std::cout << u - i << std::endl;
+
+    return 0;
+}
---
+++ src/examples/002Chapter/2-1-2-type-conversions/type_conversion.h
@@ -0,0 +1,7 @@
+bool get_boolean_value(int num);
+int get_int_val_from_bool(bool flag);
+unsigned char get_unsigned_char(int num);
+signed char get_signed_char(int num);
+int add_int_unsigned_int(int num1, unsigned int num2);
+unsigned int subtract_unsigned_int(unsigned int num1, unsigned int num2);
+int subtract_int_unsigned_int(int num1, unsigned int num2);
