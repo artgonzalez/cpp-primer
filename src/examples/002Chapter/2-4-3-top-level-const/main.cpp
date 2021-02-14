3722b3e3b12cb316497f15edcaf3294bd212eb0a
artgonzalez <artgonzalezjr@gmail.com>
2021-02-13 19:33:12 -0600

    243 const top-level

---
+++ src/examples/002Chapter/2-4-3-top-level-const/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main()
+{
+    int i = 0;
+    int *const p1 = &i; // we can't change the value of p1; const is top-level
+    const int ci = 42; // we cannot change ci; const is top-level
+    const int *p2 = &ci; // we can change p2; const is low-level
+    const int *const p3 = p2; // right-most const is top-level, left-most is not
+    const int &r = ci; // const in reference types is always low-level
+
+    p2 = &i;
+
+    std::cout<<*p1<<"\n";
+    std::cout<<ci<<"\n";
+    std::cout<<*p2<<"\n";
+    std::cout<<*p3<<"\n";
+    std::cout<<r;
+
+    return 0;
+}
