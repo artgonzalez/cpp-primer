a2ea8a44f233a8ee3edd8570a93b553bdd001c61
artgonzalez <artgonzalezjr@gmail.com>
2021-02-13 19:16:05 -0600

    241 reference to const

--- src/examples/002Chapter/2-4-1-const-reference/main.cpp
+++ src/examples/002Chapter/2-4-1-const-reference/main.cpp
@@ -7,5 +7,13 @@ int main()
     std::cout<<global_const<<"\n";
     //local_const not accessible
     use_extern_const();
+
+    const int num = 512;
+    const int &ref_num = num;//can't modify via ref_num const
+    
+    int num2 = 256;
+    const int &ref_num1 = num2;//const ref to non const int; ref read only
+    
+
     return 0;
 }
