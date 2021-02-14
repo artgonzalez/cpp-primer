82f00d0569aeef50fbb5e24489074b5198ceda09
artgonzalez <artgonzalezjr@gmail.com>
2021-02-13 23:00:07 -0600

    263 sales data header guards

--- inc/sales_data.h
+++ inc/sales_data.h
@@ -1,3 +1,5 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
 #include<string>
 
 struct SalesData
@@ -6,3 +8,5 @@ struct SalesData
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
+
+#endif
---
+++ src/examples/002Chapter/2-6-3-header-files/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "sales_data.h"
+
+int main()
+{
+    SalesData data;
+
+    std::cout<<data.book_no;
+
+    return 0;
+}
