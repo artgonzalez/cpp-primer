aba63b085dc0a073c1391b66ac205ed990a15ca2
artgonzalez <artgonzalezjr@gmail.com>
2021-02-14 12:42:27 -0600

    332 add elements to vector

---
+++ src/examples/003Chapter/3-3-2-vector-add/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;  using std::vector;
+
+int main()
+{
+    vector<int> nums;//empty
+    nums.push_back(5);//add 5 to vector elements 
+
+    cout<<nums[0];
+
+    return 0;
+}
