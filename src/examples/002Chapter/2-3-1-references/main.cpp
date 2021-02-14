bc00350c44ba39152b94fb5b0a06d877d8186e10
artgonzalez <artgonzalezjr@gmail.com>
2021-02-13 18:12:47 -0600

    231 references exercise

--- src/examples/002Chapter/2-3-1-references/main.cpp
+++ src/examples/002Chapter/2-3-1-references/main.cpp
@@ -4,12 +4,17 @@ int main()
 {
     int num = 512;
     int &num_ref = num;//alias to num==reference
-
+    
     std::cout<<num<<"\n";
     std::cout<<num_ref<<"\n";
     num_ref = 256;
 
-    std::cout<<num;
+    std::cout<<num<<"\n";
 
+    int &ref = num_ref;
+    std::cout<<ref<<"\n";
+    
+    int i = ref;
+    std::cout<<i<<"\n";
     return 0;
 }
