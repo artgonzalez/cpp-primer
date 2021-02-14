75d040e29fd51016f62a735dfaf0845bc1126426
artgonzalez <artgonzalezjr@gmail.com>
2021-02-14 00:20:48 -0600

    301 using namespace statments

---
+++ src/examples/003Chapter/3-1-using/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// DO NOT USE USING STATEMENTS IN HEADER FILES
+//THAT WILL COPY THOSE USINGS TO ALL FILES INCLUDING HEADER
+//MAY CAUSE NAME CONFLICTS
+//using statements for standard library object use
+using std::cout;
+
+int main()
+{
+    cout<<"Hello World!";
+
+    return 0;
+}
