5d7e22d0c17cd6f6f56dfba851c2f73ac5cc3fd5
artgonzalez <artgonzalezjr@gmail.com>
2021-02-18 20:09:19 -0600

    52 control structures statement scope

---
+++ src/examples/005Chapter/5-2-statement-scope/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main()
+{
+    for(auto i=0; i < 3; ++i)
+    {
+        cout<<i<<endl;
+    }
+    //i is not accessible here
+
+
+    return 0;
+}
