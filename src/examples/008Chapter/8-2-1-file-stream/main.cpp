1c51920a9301db00e927f05fb9fb577f36102f54
artgonzalez <artgonzalezjr@gmail.com>
2021-02-24 15:24:02 -0600

    821 fstream files

---
+++ src/examples/008Chapter/8-2-1-file-stream/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout; using std::ofstream; using std::ifstream;
+using std::string;
+
+int main()
+{
+    string file = "file.data";
+
+    ofstream out_file(file);
+
+    out_file<<"John ";
+    out_file<<"Joe ";
+    out_file<<"Mary ";
+
+    out_file.close();
+
+    ifstream in_file(file);
+
+    string name;
+
+    while(in_file>>name)
+    {
+        cout<<name<<"\n";
+    }
+
+    in_file.close();
+
+    return 0;
+}
