352281f20b20c7aa92e64c503dbe9a67b07a7d47
artgonzalez <artgonzalezjr@gmail.com>
2021-02-25 21:36:35 -0600

    955 string to int double

---
+++ src/examples/009Chapter/9-5-5-num-conv/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using std::cout; using std::string;
+
+int main()
+{
+    int num = std::stoi("10");
+    cout<<num<<"\n";
+
+    double num1 = std::stod("10.9");
+    cout<<num1<<"\n";
+
+    return 0;
+}
